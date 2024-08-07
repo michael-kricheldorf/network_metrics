0a88801aa4a79c06fc064ec096c98402157d1492
michael-kricheldorf <jrkrich@gmail.com>
2024-08-07 10:13:24 -0400

    added ping timer setting folder

--- setup.sh
+++ setup.sh
@@ -7,9 +7,11 @@
 
 # ------ ALL -------
 # edit systemd unit files to current working directory
+# clear databases
 
 # ------ FOR PING SETUP ------
 # take gateway name as argument and copy/paste that file as ./ping_config.json
+# also use gateway name to locate appropriate ping timer unit file
 
 # make custom ping and modem times for each gateway so that they don't
 # interfere, and so that Norvel and Mia's are synced to after Mia
